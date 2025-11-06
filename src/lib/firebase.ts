dc33dc1a4abc0536d3286e166e7f2056b1673a2f
Challanges Vedio <challangesvedio@gmail.com>
2025-11-06 05:28:29 +0000

    sign in with google gives error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "test-api-key",
-  authDomain: "test-auth-domain",
-  projectId: "test-project-id",
-  storageBucket: "test-storage-bucket",
-  messagingSenderId: "test-messaging-sender-id",
-  appId: "test-app-id",
+  apiKey: "AIzaSyC2a36b-gPAO7qk-r7j4voAYsdadxuaSNI",
+  authDomain: "dev-prototyper-2321.firebaseapp.com",
+  projectId: "dev-prototyper-2321",
+  storageBucket: "dev-prototyper-2321.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:a1b2c3d4e5f6a7b8c9d0e1",
 };
 
 // Initialize Firebase
